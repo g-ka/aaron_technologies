61f9e21cd984466de9040268822cc2d91540abec
g-ka <gkaphilathithiya@gmail.com>
2023-10-16 17:53:53 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Courses from "./components/Courses";
 import Articles from "./components/Articles";
 import About from "./components/About";
 import Footer from "./components/Footer";
-import { Route , Routes } from 'react-router-dom';
+import { Route , Routes , Navigate } from 'react-router-dom';
 
 function App() {
 
