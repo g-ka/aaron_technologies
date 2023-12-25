1de23d7c053720030e1648a7920bf0e926fdac83
g-ka <gkaphilathithiya@gmail.com>
2023-12-25 13:52:16 +0530

    logo192_update

--- src/components/contact_components/ContactForm.js
+++ src/components/contact_components/ContactForm.js
@@ -28,39 +28,37 @@ const ContactForm = () => {
   const send_style = {backgroundColor: bg_color};
 
   return (
-    <>
-      <section className="contact_section" id="contact_us">
-        <form 
-          className="contact_section__card"
-          ref={form}
-          onSubmit={sendEmail}
-        >
-          <div className="contact_section__card__desc">
-            <h3>
-              Share your thoughts with us
-            </h3>
-            <p>
-              Mention your message as briefly as possible
-            </p>
-            <input name="from_name" type="text" className="common" placeholder="Your Name" required />
-            <input name="form_email" type="email" className="common" placeholder="Your E-mail" required />
-            <input name="subject" type="text" className="common" placeholder="Subject" required />
-            <textarea name="message" className="message" placeholder="Share your message here" rows="8" required />
-          </div>
-          <div className="contact_section__card__img">
-            <img src={contact_form} alt="kapfrost_contact_us" />
-            <button 
-              type='submit' 
-              style={send_style} 
-              onMouseOver={() => set_bg_color('black')}
-              onMouseLeave={() => set_bg_color('rgb(87, 98, 253)')}
-            >
-              {text}
-            </button>
-          </div>
-        </form>
-      </section>
-    </>
+    <section className="contact_section" id="contact_us">
+      <form 
+        className="contact_section__card"
+        ref={form}
+        onSubmit={sendEmail}
+      >
+        <div className="contact_section__card__desc">
+          <h3>
+            Share your thoughts with us
+          </h3>
+          <p>
+            Mention your message as briefly as possible
+          </p>
+          <input name="from_name" type="text" className="common" placeholder="Your Name" required />
+          <input name="form_email" type="email" className="common" placeholder="Your E-mail" required />
+          <input name="subject" type="text" className="common" placeholder="Subject" required />
+          <textarea name="message" className="message" placeholder="Share your message here" rows="8" required />
+        </div>
+        <div className="contact_section__card__img">
+          <img src={contact_form} alt="kapfrost_contact_us" />
+          <button 
+            type='submit' 
+            style={send_style} 
+            onMouseOver={() => set_bg_color('black')}
+            onMouseLeave={() => set_bg_color('rgb(87, 98, 253)')}
+          >
+            {text}
+          </button>
+        </div>
+      </form>
+    </section>
   )
 }
 
